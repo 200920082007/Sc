9effb9ff13d77149324e1765fff4d044d85cffe6
NEGANTG <90514589+NEGANTG@users.noreply.github.com>
2023-04-16 11:29:04 +0530

    Update add2group.py

--- add2group.py
+++ add2group.py
@@ -103,7 +103,7 @@ n = 0
 for user in users:
     n += 1
     if n % 50 == 0:
-	    time.sleep(900)
+	    time.sleep(30)
 	    try:
 	        print ("Adding {}".format(user['id']))
 	        if mode == 1:
